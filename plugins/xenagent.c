1a0c3d17922de355d2aa4ccd6de017924c48e071
Fira <loyauflorian@gmail.com>
2014-09-14 19:59:51 +0200

    Added ID and Dom0/U check

--- plugins/xenagent.c
+++ plugins/xenagent.c
@@ -30,12 +30,24 @@ int main(void) {
 	printf("total_memory\t%llu\n", xenstat_node_tot_mem(node));
 	printf("free_memory\t%llu\n", xenstat_node_free_mem(node));
 //	printf("freeable_memory\t%li\n", xenstat_node_freeable_mb(node));
+	printf("cpu_amount\t%i\n", xenstat_node_num_cpus(node));
+	printf("cpu_speed\t%llu\n", xenstat_node_cpu_hz(node));
 
-/*
 	int domains_amount = xenstat_node_num_domains(node);
 	if(!domains_amount) exit(0);
+	printf("domains\t%i\n", domains_amount);
+
 	xenstat_domain** domains = malloc(sizeof(xenstat_domain*) * domains_amount);
-*/	
+	
+
+	for(int i=0; i<domains_amount; i++) {
+		xenstat_domain* domain = xenstat_node_domain_by_index(node, i);
+		if(xenstat_domain_id(domain)) {
+			printf("<<<<%s>>>>\n", xenstat_domain_name(domain));
+		} else{ printf("<<<<>>>>\n"); }
+		printf("<<<xen_domain>>>\n");
+		printf("id\t%u\n", xenstat_domain_id(domain));
+	}
 
 	return 0;
 }
